4ca90ac957eb4afa4db7eb21d64f0ffe1e69700c
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-01-30 03:17:34 -0500

    added a few more queries

--- Queries/basic_queries.sql
+++ Queries/basic_queries.sql
@@ -26,7 +26,7 @@ Select 'Books Borrowed' AS Description, count(loanID) AS 'Total' from BookLoans;
 /* (RESULTS): 	
 Description   | Total
 Members       | 200
-Books		  | 930
+Books         | 930
 Books Borrowed| 0  (Database has 0 values as of right now
 */
 
@@ -73,5 +73,28 @@ where year (publication_date) < 1950; */
 -- (RESULTS): total books | 57
 
 
+-- Query shows the title of book and how many copies where copies are less than or equal to 3
+/*select title as 'Book Title', available_copies as 'Book Copies' from Books
+where available_copies <= 3
+order by available_copies desc; */
+-- (RESULTS) shows a table of books tittles and how many copies are available in decending order based on copies
 
 
+-- Query shows avg number of available copies for per category 
+/*Select category as 'Category', round(avg(available_copies),1) as 'Copies' from Books
+group by category
+order by round(avg(available_copies), 1) desc; */
+-- (RESULT): table with category and copies rounded (ex: cookbook | 5.6)
+
+-- Query shows how many members are in each area code ordered by most in the same area code
+/* select count(member_name), phone_number from LibraryMembers
+group by phone_number
+order by count(member_name) desc; */
+-- (RESULT): table that shows how many members that are in each area code
+
+
+-- Query that shows the month and how many members have joined that month
+/*Select count(memberID), month(membership_date) from LibraryMembers
+group by month(membership_date)
+order by count(memberID) desc; */
+-- (RESULT): table with memberID count and the month (Jan has the most followed by Aug)
