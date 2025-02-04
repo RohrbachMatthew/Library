0d4cb27978780e72ad15d2f1e2a641a7e2043f75
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-02-04 03:05:45 -0500

    adding folders and new files import_export and backup folders and files

---
+++ Import_Export_database/export_data_csv.sql
@@ -0,0 +1,13 @@
+/* 
+This file creates a csv file of the data in the selected table
+	- Change Books to desired table
+    - Replace '*****' with output file path and name of file to be created
+*/
+
+
+/*SELECT * FROM Books
+INTO OUTFILE '***************' -- Enter path to file and file name ('C:/user/sql/new_file_name.csv')
+fields terminated by ',' -- each column is seperated by a comma
+enclosed by '"' -- each field enclosed by double quote
+lines terminated by '/n'; -- rows end with new line*/
+
